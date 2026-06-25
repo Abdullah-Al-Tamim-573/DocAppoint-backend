66bb912135e3d5ca0aeb7fe234a99313e9d8bb56
Abdullah-Al-Tamim-573 <abdullahaltamim573@gmail.com>
2026-06-25 13:11:24 +0600

    add jwt and implement it all dynamicroutes like /doctors/:id and /doctorAppointments/:id

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
+
 
 
 
@@ -21,6 +23,36 @@ const client = new MongoClient(uri, {
     }
 });
 
+const jwks = createRemoteJWKSet(
+    
+    new URL("https://docappoint-573.vercel.app/api/auth/jwks")
+    // new URL("http://localhost:3000/api/auth/jwks")
+)
+
+// verify token 
+const verifyToken = async (req, res, next) => {
+    let authHeader = req?.headers?.authorization;
+    if(!authHeader) {
+       return res.status(401).json({message : "unauthorized"})
+    }
+    let token = authHeader.split(" ")[1];
+    if(!token) {
+        return res.status(401).json({message : "unauthorized"})
+    }
+
+    try {
+        const {payload} = await jwtVerify(token, jwks);
+        
+         next()
+    }
+    catch(error) {
+        console.log(error, "payload error")
+        return res.status(403).json({message : "forbidden"})
+    }
+   
+   
+}
+
 async function run() {
     try {
 
@@ -29,7 +61,7 @@ async function run() {
 
         const db = client.db("docAppoint573");
         const doctorsCollection = db.collection("doctors");
-        const appointmentsCollection = db.collection("appointments")
+        const appointmentsCollection = db.collection("appointments");
 
         // doctors 
         app.get("/doctors", async (req, res) => {
@@ -39,12 +71,15 @@ async function run() {
         })
 
         // single doctor 
-        app.get('/doctors/:id', async (req, res) => {
-            let { id } = req.params;
-            const query = { _id: new ObjectId(id) };
-            const result = await doctorsCollection.findOne(query);
-            res.send(result)
-        })
+        app.get('/doctors/:id',
+           verifyToken,
+            async (req, res) => {
+                let { id } = req.params;
+
+                const query = { _id: new ObjectId(id) };
+                const result = await doctorsCollection.findOne(query);
+                res.send(result)
+            })
 
 
         // doctor appointments post 
@@ -65,8 +100,8 @@ async function run() {
         })
 
         // single appointment doctor
-        app.get('/doctorAppointments/:id', async (req, res) => {
-            let {id} = req.params;
+        app.get('/doctorAppointments/:id', verifyToken ,async (req, res) => {
+            let { id } = req.params;
             const query = { _id: new ObjectId(id) };
             const result = await appointmentsCollection.findOne(query);
             res.send(result)
@@ -74,25 +109,25 @@ async function run() {
 
         // appointment doctor delete (single doctor delete)
         app.delete('/doctorAppointments/:id', async (req, res) => {
-             let {id} = req.params;
-             let query = {_id : new ObjectId(id)};
-             let result = await appointmentsCollection.deleteOne(query);
-             res.send(result)
+            let { id } = req.params;
+            let query = { _id: new ObjectId(id) };
+            let result = await appointmentsCollection.deleteOne(query);
+            res.send(result)
         })
 
         app.patch('/doctorAppointments/:id', async (req, res) => {
-               let {id} = req.params;
-               let upgradedData = req.body;
-               let query = {_id: new ObjectId(id)};
-               let upgradeDoc = {
+            let { id } = req.params;
+            let upgradedData = req.body;
+            let query = { _id: new ObjectId(id) };
+            let upgradeDoc = {
                 $set: {
-                   ...upgradedData
+                    ...upgradedData
                 }
-               }
-              let result = await appointmentsCollection.updateOne(query, upgradeDoc);
-              res.send(result)
+            }
+            let result = await appointmentsCollection.updateOne(query, upgradeDoc);
+            res.send(result)
         })
-        
+
 
 
 
